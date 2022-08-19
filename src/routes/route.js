66773a8c43f21061cd67338e4724a40ddf858351
chandan_pandey <chandankpandey75@gmail.com>
2022-08-19 14:37:44 +0530

    commiting last assy

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const UserController= require("../controllers/userController")
 const BookController= require("../controllers/bookController")
 
-router.get(" ", function (req, res) {
+router.get("/test-me ", function (req, res) {
     res.send("My first ever api!") 
 })
 
